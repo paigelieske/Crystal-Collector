6aa95bff70b53c141b4949af233a3796167e8614
Paige Lieske <paigeproctor@comcast.net>
2019-03-04 14:43:06 -0500

    added notes, changed name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+    // creates random target number
     var computerGuess = Math.floor(Math.random() * 101+19);
     $(".randomNumber").text(computerGuess);
     console.log("Target: " + computerGuess);
 
+    // variables for random gem values
     var randomGemOne = Math.floor(Math.random() * 11+1);
     var randomGemTwo = Math.floor(Math.random() * 11+1);
     var randomGemThree = Math.floor(Math.random() * 11+1);
     var randomGemFour = Math.floor(Math.random() * 11+1);
 
+    // variables for gem totals, wins, and losses
     var userTotal = 0;
     var wins = 0;
     var losses = 0;
 
+    //game reset after win or loss
     function reset () {
         console.clear();
         computerGuess = Math.floor(Math.random() * 101+19);
@@ -23,7 +27,7 @@ $(document).ready(function() {
         randomGemFour = Math.floor(Math.random() * 11+1);
         $(".total").text(userTotal=0);
     }
-    
+    //win function
     function congrats () {
         wins++;
         $(".totalWins").text("Wins: " + wins);
@@ -31,6 +35,7 @@ $(document).ready(function() {
         reset();
     }
 
+    //loss function
     function tryAgain () {
         losses++;
         $(".totalLosses").text("Losses: " + losses);
@@ -38,6 +43,8 @@ $(document).ready(function() {
         reset();
     }
 
+    //adds together the values of each gem click, placing the running total inside the userTotal variable
+    //provides instructions one the userTotal matches or surpasses the target
     $("#one").on("click", function () {
         console.log("Green = " + randomGemOne);
         userTotal = randomGemOne + userTotal;
